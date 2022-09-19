49d99ee1d99814307cc2e5cf83ce77eea182b010
Baltazar Correa <baltazarcorrea299@gmail.com>
2022-09-19 18:14:44 -0300

    Aviso de fuera de linea

--- DOM.js
+++ DOM.js
@@ -1,14 +1,35 @@
 let contador=0;
+let tester;
 let tt=0;
 let bb=0;
 let cc=0;
+let dd=1;
+let ff=0;
 let x=43; //de 0 a 28
 let y=13; // de 0 a 88
 
-
+window.addEventListener("offline", a=>{
+    let e =document.getElementById("offline");
+    console.log(e.style.zIndex);
+    e.style.zIndex=110;
+    setTimeout(()=>{
+        e.style.zIndex=90;
+    },2000);
+})
+window.addEventListener("online", a=>{
+    let e =document.getElementById("online");
+    console.log(e.style.zIndex);
+    e.style.zIndex=110;
+    setTimeout(()=>{
+        e.style.zIndex=90;
+    },2000);
+})
+document.addEventListener("DOMContentLoaded", b=>{
+    theme();
+    comprobar();
+})
 document.addEventListener("scroll" ,pp=>{
     let scroll = document.documentElement.scrollTop;
-    console.log(scroll);
     let ele= document.getElementById("bot");
     if(scroll<200){
         ele.setAttribute("class", "boton subir hidden");
@@ -79,7 +100,6 @@ function reloj(){
 }
 function freno(){
     tt=1;
-    
 }
 function alarma(){
     var tempo=setInterval(()=>{
@@ -141,6 +161,7 @@ function oscuro(){
         let cuerpo= document.getElementById("cuerpo");
         cuerpo.setAttribute("class", "nocturno");
         cc++;
+        localStorage.setItem("tema", 0);
         let bot= document.getElementById("noche");
         bot.textContent="🌞";
     }
@@ -148,7 +169,73 @@ function oscuro(){
         let cuerpo= document.getElementById("cuerpo");
         cuerpo.setAttribute("class", "dia");
         cc=0;
+        localStorage.setItem("tema", 1);
         let bot= document.getElementById("noche");
         bot.textContent="🌙";
     }
 }
+function theme(){
+    if(localStorage.getItem("tema")===null){
+        localStorage.setItem("tema", 0);
+    }
+    if(localStorage.getItem("tema")==0){
+        let cuerpo= document.getElementById("cuerpo");
+        cuerpo.setAttribute("class", "nocturno");
+        localStorage.setItem("tema", 0);
+        cc=1;
+        let bot= document.getElementById("noche");
+        bot.textContent="🌞";
+    }
+    else if(localStorage.getItem("tema")==1){
+        let cuerpo= document.getElementById("cuerpo");
+        cuerpo.setAttribute("class", "dia");
+        localStorage.setItem("tema", 1);
+        cc=1;
+        let bot= document.getElementById("noche");
+        bot.textContent="🌙";
+    }
+}
+function comprobar(){
+    let vid=document.getElementById("vid");
+    let videos=document.getElementById("videos");
+    let maps=document.getElementById("maps");
+    let mapas=document.getElementById("mapas");
+    let ee=document.getElementById("responsives");
+    let link=document.getElementById("link")
+
+    if(window.innerWidth<=800){
+        ee.removeChild(vid);
+        ee.removeChild(maps);
+        dd=0;
+        link.insertAdjacentHTML("afterbegin", '<a id="videos" href="https://www.youtube.com/watch?v=WS6hnh7s7UI&ab_channel=Archivoschistosones2009"> video</a>');
+        link.insertAdjacentHTML("afterbegin", '<a id="mapas" href="https://www.google.com.ar/maps/@-31.3366025,-64.2779967,15z">Mapa</a><br>');
+    }
+    else if(window.innerWidth>800 && dd===0){
+        console.log(videos);
+        link.removeChild(videos);
+        link.removeChild(mapas);
+
+        ee.insertAdjacentHTML("afterbegin", '<div class="vid" id="vid"></div>');
+        let vid=document.getElementById("vid");
+        vid.insertAdjacentHTML("afterbegin",'<iframe width="560" height="315" id="vide" src="https://www.youtube.com/embed/WS6hnh7s7UI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+
+        ee.insertAdjacentHTML("afterbegin", '<div class="maps" id="maps"></div>');
+        let maps=document.getElementById("maps");
+        maps.insertAdjacentHTML("afterbegin",'<iframe class="map" id="map" src="https://www.google.com/maps/embed?    pb=!1m14!1m12!1m3!1d13631.2425274672!2d-64.27799675!3d-31.336602499999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.    1!5e0!3m2!1ses-419!2sar!4v1663243676434!5m2!1ses-419!2sar" width="600" height="450" style="border:0;"   allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>')
+        dd=1;
+    }
+}
+function ventanas(){ 
+    event.preventDefault();
+    let $form = document.getElementById("form");
+    if(ff===0){
+        tester = window.open($form.url.value, "tester", 'innerWidth='+$form.ancho.value+', innerHeight='+$form.alto.value);
+        console.log(tester);
+        ff=1;
+    }
+    else if(ff===1){
+        tester.close();
+        ff=0;
+    }
+}
+console.log(navigator.onLine);
